cf3fa62f22b99db96e9f8472382589dd6e50ad4b
Mehdi GUERRIB <mehdi.guerrib.dz@viacesi.fr>
2021-05-21 17:27:51 +0100

    rigliha

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const app = express()
 /**
  * request body decoder middleware
  */
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
-app.use('/public',express.static('./static'))
+app.use('/public', express.static('./static'))
 
 
 /**
@@ -24,16 +24,18 @@ app.use(ejsSession)
  */
 app.set('view engine', 'ejs')
 
-
+app.get('/', (req, res) => {
+    res.render('pages/accueil')
+})
 /**
  * login, logout & home route
 */
-app.use('',routes.baseRoute)
+app.use('', routes.baseRoute)
 
 /**
  * launch the server
  */
-app.listen(process.env.PORT||3000,(err)=>{
-    if(err) return console.log(err)
-    console.log("listening on http://localhost:"+ (process.env.PORT || 3000))
+app.listen(process.env.PORT || 3000, (err) => {
+    if (err) return console.log(err)
+    console.log("listening on http://localhost:" + (process.env.PORT || 3000))
 })
